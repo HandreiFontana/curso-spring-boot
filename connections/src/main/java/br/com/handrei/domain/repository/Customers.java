7f0decebe14d12d6bebb72f5aace538136e11b15
HandreiFontana <HandreiFontana>
2023-07-12 20:09:42 -0300

    refact: all functions changed to jpa entity manager

--- connections/src/main/java/br/com/handrei/domain/repository/Customers.java
+++ connections/src/main/java/br/com/handrei/domain/repository/Customers.java
@@ -2,25 +2,15 @@ package br.com.handrei.domain.repository;
 
 import br.com.handrei.domain.entity.Customer;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Repository
 public class Customers {
-
-    private static String UPDATE = "UPDATE customers SET name = ? WHERE id = ?";
-    private static String SELECT_ALL = "SELECT * FROM customers";
-    private static String DELETE = "DELETE FROM customers WHERE id = ?";
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     private EntityManager entityManager;
@@ -31,39 +21,36 @@ public class Customers {
         return customer;
     }
 
+    @Transactional
     public Customer update(Customer customer) {
-        jdbcTemplate.update( UPDATE, new Object[]{customer.getName(), customer.getId()} );
+        entityManager.merge(customer);
         return customer;
     }
 
+    @Transactional(readOnly = true)
     public List<Customer> list() {
-        return jdbcTemplate.query(SELECT_ALL, getRowMapper());
+        return entityManager
+                .createQuery("FROM Customer", Customer.class)
+                .getResultList();
     }
 
+    @Transactional(readOnly = true)
     public List<Customer> list(String search) {
-        return jdbcTemplate.query(
-                SELECT_ALL.concat(" WHERE name LIKE ?"),
-                new Object[]{"%" + search + "%"},
-                getRowMapper()
-        );
+        String jpql = " SELECT c FROM Customer c WHERE c.name like :name ";
+        TypedQuery<Customer> query = entityManager.createQuery(jpql, Customer.class);
+        query.setParameter("name", "%" + search + "%");
+        return query.getResultList();
     }
 
-    private static RowMapper<Customer> getRowMapper() {
-        return new RowMapper<Customer>() {
-            @Override
-            public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Integer id = rs.getInt("id");
-                String name = rs.getString("name");
-                return new Customer(id, name);
-            }
-        };
-    }
-
+    @Transactional
     public void delete(Customer customer) {
-        delete(customer.getId());
+        customer = entityManager.contains(customer) ? customer : entityManager.merge(customer);
+        entityManager.remove(customer);
     }
 
+    @Transactional
     public void delete(Integer id) {
-        jdbcTemplate.update( DELETE, new Object[]{id} );
+        Customer customer = entityManager.find(Customer.class, id);
+        delete(customer);
     }
 }
